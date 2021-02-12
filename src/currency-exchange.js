035dd290d30fb357aed6d794216b725391e7c3ff
Kerry Lang <klang812@gmail.com>
2021-02-12 09:24:06 -0800

    add service files and get API

---
+++ src/currency-exchange.js
@@ -0,0 +1,14 @@
+export default class CurrencyExchange {
+  static getExchange(country) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${country}`)
+    .then(function(response) {
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    });
+  }
+}
