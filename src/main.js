21b0348f3cf396ba49bda0543915102d1042a54f
Kerry Lang <klang812@gmail.com>
2021-02-12 09:39:52 -0800

    build html

--- src/main.js
+++ src/main.js
@@ -3,3 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './currency-exchange.js';
+
+$(document).ready(function() {
+  $()
+}
