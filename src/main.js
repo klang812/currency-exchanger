9d389f14f23e4186e474e0d2121ee887d082dccc
Kerry Lang <klang812@gmail.com>
2021-02-12 10:33:05 -0800

    research parsing current API

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './currency-exchange.js';
 
+let usDollar = parseInt($('#usDollar').val());
+function getExchange(response) {
+  if (response.conversion_rates.EUR) {
+    $('.showConversion').text(`${usDollar} is worth ${response.conversion_rates.EUR}`);
+
+  } else if (response.conversion_rates.THB) {
+    $('.showConversion').text(`${usDollar} is worth ${response.conversion_rates.THB}`);
+
+  } else (response.conversion_rates.KRW) {
+  $('.showConversion').text(`${usDollar} is worth ${response.conversion_rates.KRW}`);
+  }
+};
+
 $(document).ready(function() {
-  $()
-}
+  $('#getConversion').click(function() {
+    CurrencyExchange.getExchange(country)
+      .then(function(response) {
+        getExchange(response);
+      });
+  });
+});
