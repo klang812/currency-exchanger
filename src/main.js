5fe3e63cc5ecf093c277e8f33327b40409e247c8
Kerry Lang <klang812@gmail.com>
2021-02-12 16:15:05 -0800

    fix template literal in API call

--- src/main.js
+++ src/main.js
@@ -8,16 +8,16 @@ import CurrencyExchange from './js/currency-exchange.js';
 function getConversion(response) {
   let usDollar = parseInt($('#usDollar').val());
   if (response.target_code) {
+    console.log(response);
     $('.showConversion').text(`${usDollar} is worth ${response.conversion_rate}`);
   } else  {
-    $('.showError').text(`There was an error: ${response["error-type"]}`);
+    $('.showError').text(`There was an error: ${response.error}`);
   }
 }
 
 $(document).ready(function() {
   $('#getConversion').click(function() {
-    let country = document.getElementsByName("country");
-    console.log(country);
+    let country = $('input:radio[name=country]:checked').val();
     CurrencyExchange.getExchange(country)
       .then(function(response) {
         getConversion(response);
